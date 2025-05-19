6c50618065872587b1b8d03c36382cba25063a3e
ExolutionTech <exolutionza@gmail.com>
2025-05-19 02:20:14 +0200

    .

--- legacy/caseon-mono/src/main.jsx
+++ legacy/caseon-mono/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import '@/services/firebase'; // Import Firebase initialization
+// 
 
 const root = createRoot(document.getElementById('root'));
 root.render(
