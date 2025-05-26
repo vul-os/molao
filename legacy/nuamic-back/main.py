48cf202fe121d3f4d8b0d1a0cbbaa4c36cbdf037
travis <travisgeorge707@gmail.com>
2025-05-26 10:48:47 +0200

    cors update to fast api server

--- legacy/nuamic-back/main.py
+++ legacy/nuamic-back/main.py
@@ -86,6 +86,23 @@ else:
 # ---------- FastAPI App ----------
 app = FastAPI()
 
+# --- Add CORS Middleware ---
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5176"
+    # ,
+    # "https://nuamic.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # ---------- Request & Response Models ----------
 class EmbeddingRequest(BaseModel):
     input: List[str]
