4b81c90bcd67ccfb3970648b95406e8f61ae4f02
TravisGeorge707 <88790591+TravisGeorge707@users.noreply.github.com>
2025-06-02 12:24:02 +0200

    srta

--- legacy/nuamic-back/main.py
+++ legacy/nuamic-back/main.py
@@ -219,5 +219,4 @@ def read_root():
 # ---------- Entrypoint ----------
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
